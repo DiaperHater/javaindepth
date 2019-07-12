00367852e823bee9f07adcb4bf02f755a2a6c3b7
314ronin <pownmymail@gmail.com>
2019-07-12 14:35:03 +0300

    modified:   src/com/semanticsquare/basics/HelloWorld.java

--- src/com/semanticsquare/basics/HelloWorld.java
+++ src/com/semanticsquare/basics/HelloWorld.java
@@ -1,13 +1,15 @@
+package com.semanticsquare.basics;
+
 public class HelloWorld{
 	
 
 	public static void main(String[] args){
 		System.out.println("Hello world");
-		Beatch b = new Beatch();
+		Bitch b = new Bitch();
 		System.out.println(b.age);
 	}
 }
 
-class Beatch{
+class Bitch {
 	int age = 90;
 }
