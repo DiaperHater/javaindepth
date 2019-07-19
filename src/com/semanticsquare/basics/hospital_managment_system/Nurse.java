c5c4894c1c120bcc7c35ec23974bbf54f52cc277
314ronin <pownmymail@gmail.com>
2019-07-19 14:37:05 +0300

    Coding exercise 6: HospitalManagmentSystem is done

---
+++ src/com/semanticsquare/basics/hospital_managment_system/Nurse.java
@@ -0,0 +1,13 @@
+package com.semanticsquare.basics.hospital_managment_system;
+
+public class Nurse extends Staff {
+    private long nurseId;
+
+    public long getNurseId() {
+        return nurseId;
+    }
+
+    public void setNurseId(long nurseId) {
+        this.nurseId = nurseId;
+    }
+}
