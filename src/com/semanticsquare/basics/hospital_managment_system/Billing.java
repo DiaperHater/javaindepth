2330165e820acb5a5424767fced2a53061b51539
iggy <elizaveta@MacBook-Pro-li.local>
2019-07-25 01:40:39 +0300

    Oops your solution is incorect null

--- src/com/semanticsquare/basics/hospital_managment_system/Billing.java
+++ src/com/semanticsquare/basics/hospital_managment_system/Billing.java
@@ -2,24 +2,28 @@ package com.semanticsquare.basics.hospital_managment_system;
 
 public class Billing {
 
+    private static final double DISCOUNT_PLATINUM = 50;
+    private static final double DISCOUNT_GOLD = 40;
+    private static final double DISCOUNT_SILVER = 30;
+    private static final double DISCOUNT_BRONZE = 25;
+
     public static double[] computePaymentAmount(Patient patient, double amount) {
         double[] payments = new double[2];
         HealthInsurancePlan patientInsurancePlan = patient.getInsurancePlan();
 
+        payments[0] = amount * patientInsurancePlan.getCoverage();
+        payments[1] = amount - payments[0];
+
         if (patientInsurancePlan instanceof PlatinumPlan){
-            
-        }
-        if (patientInsurancePlan instanceof GoldPlan){
-
-        }
-        if (patientInsurancePlan instanceof SilverPlan){
-
-        }
-        if (patientInsurancePlan instanceof BronzePlan){
-
+            payments[1] -= DISCOUNT_PLATINUM;
+        } else if (patientInsurancePlan instanceof GoldPlan){
+            payments[1] -= DISCOUNT_GOLD;
+        } else if (patientInsurancePlan instanceof SilverPlan){
+            payments[1] -= DISCOUNT_SILVER;
+        } else if (patientInsurancePlan instanceof BronzePlan){
+            payments[1] -= DISCOUNT_BRONZE;
         }
 
-        // your logic
 
         return payments;
     }
